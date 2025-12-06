291c25fbc595a7195f7a8e52079853e2d8aebb40
0x2fycy3 <108025321+0x2fycy3@users.noreply.github.com>
2025-12-05 22:46:02 -0300

    change of index image alt

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <div>
       <img
         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F64.media.tumblr.com%2F66df399e80c9b05d1f0d9ded5636a6e8%2F315ffa348a2de05d-0d%2Fs1280x1920%2F8cc58b3b5f2baf13df676f5644853e79c89559a9.png&f=1&nofb=1&ipt=ff7fd23e54629e8fd884b7c75ae68c73792e673c433a14b4aac128d37aac43b7"
-        alt="Img"
+        alt="Image"
         style={{ maxWidth: "100%", height: "auto" }}
       />
     </div>
